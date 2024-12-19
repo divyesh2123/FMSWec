24e60317508c5c47f59ef99032bafafa7d5e3dde
lenovo <lenovo@LAPTOP-BLNTEBH7>
2024-12-19 14:31:37 +0530

    Add project files.

---
+++ Models/ProductProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using FMS.BusinessEntity;
+using FMS.Common;
+using FMS.Database;
+
+namespace FMS.Models
+{
+    public class ProductProfile : Profile
+    {
+        public ProductProfile()
+        {
+            CreateMap<UserRegistration, UserInfo>()
+          .ForMember(dest => dest.PasswordSalt,
+                     opt => opt.MapFrom(src => Helper.GeneratePassword(5)));
+        }
+    }
+}
